88d866039654f04efb4275cfe5cd8cb299d0cf24
Vitor Hugo <vitork42@gmail.com>
2023-08-17 06:17:14 -0300

    c

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugin: [
+      "module-resolver",
+      {
+        root: ['/src'],
+        alias: {
+          '@screens': './screens'
+        }
+     
+      }
+    ]
   };
 };
