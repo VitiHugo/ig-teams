4fe924fecb614906c697ef4907f9e7ae324849f6
vitorbarreto <vitorbarreto@bembrasil.ind.br>
2023-08-24 09:13:10 -0300

    :zap: Finished building the interface of all screens and components

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,19 @@
+import { FlatList } from "react-native";
 import { ButtonIcon } from "../../components/ButtonIcon";
+import { Filter } from "../../components/Filter";
 import { Header } from "../../components/Header";
 import { Highlight } from "../../components/Highlight";
 import { Input } from "../../components/Input";
-import { Container, Form } from "./style";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./style";
+import { useState } from "react";
+import { PlayerCard } from "../../components/PlayerCard";
+import { ListEmpty } from "../../components/ListEmpty";
+import { Button } from "../../components/Button";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState([])
+
   return (
     <Container>
       <Header showBackButton/>
@@ -22,6 +31,53 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+            name={item}
+            onRemove={() => {}}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Não há pessoas nesse time"
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 50 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+      >
+
+      </Button>
     </Container>
   )
 }
